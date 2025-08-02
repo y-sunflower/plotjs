4fa58f3007d29c1f5fa9b5171c76e6c7333612bf
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-08-02 15:40:09 +0200

    Release v0.0.3

--- plotjs/__init__.py
+++ plotjs/__init__.py
@@ -1,6 +1,6 @@
 from .main import MagicPlot
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __all__: list[str] = [
     "MagicPlot",
 ]
--- tests/test_main.py
+++ tests/test_main.py
@@ -76,4 +76,4 @@ def test_overall():
 
 
 def test_version():
-    assert plotjs.__version__ == "0.0.2"
+    assert plotjs.__version__ == "0.0.3"
