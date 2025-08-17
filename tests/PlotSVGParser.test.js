4e5c526c0b975fed82cd70cd34afbee92bf8331f
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-08-17 15:02:58 +0200

    update js tests

--- tests/PlotSVGParser.test.js
+++ tests/PlotSVGParser.test.js
@@ -66,7 +66,9 @@ test("setHoverEffect should toggle hovered class and tooltip", () => {
   const parser = new PlotSVGParser(svg, tooltip, 10, 20);
 
   const points = parser.find_points(svg, "axes_1", ["G1"]);
-  parser.setHoverEffect(points, ["Label1"], ["G1"], "block");
+
+  // Pass all required args: (points, axes_class, tooltip_labels, tooltip_groups, show_tooltip, hover_nearest)
+  parser.setHoverEffect(points, "axes_1", ["Label1"], ["G1"], "block", false);
 
   // simulate mouseover
   points.dispatch("mouseover", { bubbles: true, pageX: 100, pageY: 200 });
